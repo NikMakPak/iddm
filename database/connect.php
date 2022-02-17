b090d8f45a5e6c5dafbc37543827a2e8a76c4d71
nikmak <nicement28@gmail.com>
2022-02-17 12:10:27 +0300

    git

--- database/connect.php
+++ database/connect.php
@@ -3,7 +3,7 @@ $driver='mysql';
 $host='localhost';
 $db_name='gosuslugi';
 $db_user='root';
-// $db_password='Mn280203';
+$db_password='Mn280203';
 $charset='utf8';
 $options=[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC];
 $BASE_URL='http://localhost/iddm/';
